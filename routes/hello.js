6e680057bd78ea640301d71faa5c6c26b22523b2
Giacomo Mariani <giacomo.mariani@gmail.com>
2024-11-15 23:19:14 +0200

    serving static content and using templates

--- routes/hello.js
+++ routes/hello.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const greeting = 'greeting' in req.query ?
+    req.query.greeting :
+    'Hello';
+  res.render('hello', { greeting: greeting });
 });
 
 module.exports = router;
